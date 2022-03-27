5efca1195778ba3cb1b305765ab5e80b4c478fe7
islombeck-g <whoisit864@gmail.com>
2022-03-27 12:25:22 +0300

    sort cpp

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+
+
+void bubble_sort(std::vector<int>& arr){
+	bool is_sorted = false;
+	while (is_sorted != true) {
+		is_sorted = true;
+		for (int i = 0; i < arr.size()-1; i++) 
+			if (arr[i] > arr[i + 1]) {
+				is_sorted = false;
+				std::swap(arr[i], arr[i+1]);
+			}
+	}
+}
+
---
+++ insert_sort/insert_sort.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+void show(std::vector<int>& arr) {
+	for (int i = 0; i < arr.size(); i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+void insert_sort(std::vector<int>&arr) {
+	for (int i = 1; i < arr.size(); i++) {
+		bool sorted = false;
+		int j = i;
+		for(int j=i; (j>0) && (sorted==false); j--)
+			if (arr[j] < arr[j - 1]) 
+				std::swap(arr[j], arr[j - 1]);
+			else 
+				sorted = true;
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	std::vector<int> arr;
+	const int size = rand() % 10;
+	for (int i = 0; i < size; i++)
+		arr.push_back(rand() % 10);
+
+	show(arr);
+	insert_sort(arr);
+	show(arr);
+
+	return 0;
+}
